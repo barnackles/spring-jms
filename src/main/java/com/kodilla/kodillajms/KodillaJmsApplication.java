0a0ad3d573b56ea80e09c4686f875b28b59983a0
barnackles <p.martewicz@yahoo.com>
2023-05-16 21:14:24 +0200

    Project setup

---
+++ src/main/java/com/kodilla/kodillajms/KodillaJmsApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillajms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaJmsApplication.class, args);
+	}
+
+}
