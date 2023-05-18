be2db50323f7d92a69132a785d973315d24b98d8
barnackles <p.martewicz@yahoo.com>
2023-05-18 16:04:48 +0200

    Injecting JmsTemplate.

--- src/main/java/com/kodilla/kodillajms/controller/MessagingController.java
+++ src/main/java/com/kodilla/kodillajms/controller/MessagingController.java
@@ -1,5 +1,6 @@
 package com.kodilla.kodillajms.controller;
 
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/messages")
 public class MessagingController {
 
+    private final JmsTemplate jmsTemplate;
+
+    public MessagingController(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
     @GetMapping(path = "/process")
     public void processMessage(@RequestParam String message) {
         System.out.println("Message: " + message);
+        jmsTemplate.convertAndSend("queue-test", message);
+
     }
 
 }
