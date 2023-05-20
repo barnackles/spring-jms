9bba2065fc619a850a9221dccad7ffa82020c703
barnackles <p.martewicz@yahoo.com>
2023-05-20 18:04:30 +0200

    OrderReceiver Fix.

--- src/main/java/com/kodilla/kodillajms/domain/OrderDto.java
+++ src/main/java/com/kodilla/kodillajms/domain/OrderDto.java
@@ -6,11 +6,4 @@ import java.time.LocalDateTime;
 
 public record OrderDto(LocalDateTime created, Long productId, Long quantity) {
 
-    @JmsListener(containerFactory = "jmsFactory", destination = "queue-order")
-    public void receive(OrderDto orderDto) {
-
-        System.out.println("Received the message: " + orderDto.toString());
-
-    }
-
 }
--- src/main/java/com/kodilla/kodillajms/receiver/OrderReceiver.java
+++ src/main/java/com/kodilla/kodillajms/receiver/OrderReceiver.java
@@ -1,4 +1,14 @@
 package com.kodilla.kodillajms.receiver;
 
+import com.kodilla.kodillajms.domain.OrderDto;
+import org.springframework.jms.annotation.JmsListener;
+
 public class OrderReceiver {
+
+    @JmsListener(containerFactory = "jmsFactory", destination = "queue-order")
+    public void receive(OrderDto orderDto) {
+
+        System.out.println("Received the message: " + orderDto.toString());
+
+    }
 }
